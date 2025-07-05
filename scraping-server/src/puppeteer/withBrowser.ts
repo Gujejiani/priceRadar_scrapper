c8cbed9840e655655b14aac224da16613f157d05
kakha <kakha.gujejiani.1@iliauni.edu.ge>
2025-07-06 00:02:04 +0400

    improves compatibility, especially in containerized or restricted environment

--- scraping-server/src/puppeteer/withBrowser.ts
+++ scraping-server/src/puppeteer/withBrowser.ts
@@ -8,7 +8,11 @@ export const withBrowser = <T>(scraper: ScraperFunction<T>, options: WithBrowser
   return async (query: string): Promise<T> => {
     let browser: Browser | null = null;
     try {
-      browser = await puppeteer.launch({ headless: options.headless ?? true, devtools: false });
+      browser = await puppeteer.launch({
+        headless: options.headless ?? true,
+        devtools: false,
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      });
       const page = await browser.newPage();
 
       await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36');
